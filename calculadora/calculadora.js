bb2cd449c7a1633ce8bb5763ca2173f8406070b7
WalterRiveraGT <walterej@hotmail.es>
2022-03-25 23:02:26 -0600

    agregando conceptos básicos en js sobre parseo

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -2,5 +2,6 @@ var g_tierra = 9.8;
 var g_marte = 3.7;
 var g_jupiter = 24.8;
 var peso = prompt("ingresa tu peso ");
-peso_marte = (peso * g_marte) / g_tierra;
-document.write("tu peso en marte es: " + peso_marte);
+var peso_entero = parseFloat(peso);
+var peso_marte = parseFloat((peso_entero * g_marte) / g_tierra);
+document.write("tu peso en marte es: <strong> " + peso_marte + " kilos</strong>");
